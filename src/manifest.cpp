58d79f08313f07208503435ec40dc16d6f8730d2
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2019-01-17 15:37:21 +0900

    switch to templated version transport

--- src/manifest.cpp
+++ src/manifest.cpp
@@ -1,4 +1,4 @@
-#include <h264_image_transport/h264_subscriber.h>
+#include <h264_image_transport/decoding_subscribers.hpp>
 #include <pluginlib/class_list_macros.h>
 
-PLUGINLIB_EXPORT_CLASS(h264_image_transport::H264Subscriber, image_transport::SubscriberPlugin)
+PLUGINLIB_EXPORT_CLASS(decoding_image_transport::H264Subscriber, image_transport::SubscriberPlugin);
